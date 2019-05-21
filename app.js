e47634c66bf8a07df8e8020914752c4cf75fdef3
ArturoTorresMartinez <efrainarturotorres@gmail.com>
2019-05-21 16:14:07 -0500

    Api to react Image and item fetch

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const feedRoutes = require("./routes/feed");
 const bodyParser = require("body-parser");
+const path = require("path");
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(bodyParser.json());
+app.use("/images", express.static(path.join(__dirname, "images")));
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Contro-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
@@ -11,4 +14,18 @@ app.use((req, res, next) => {
   next();
 });
 app.use("/feed", feedRoutes);
-app.listen(8080);
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://arthur:poq7283ipod@cluster0-e7jon.mongodb.net/messages?retryWrites=true"
+  )
+  .then(result => {
+    app.listen(8080);
+  })
+  .catch(err => console.log(err));
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,14 +1,68 @@
+const { validationResult } = require("express-validator/check");
+const Post = require("../models/post");
+
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [{ title: "First Post", content: "This is the first posrt!" }]
-  });
+  Post.find()
+    .then(p => {
+      res.status(200).json({ message: "Done deal", posts: p });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation Failed, entered data incorrect");
+    error.statusCode = 422;
+    throw error;
+  }
   const title = req.body.title;
   const content = req.body.content;
-  res.status(201).json({
-    message: "Post created",
-    post: { id: new Date().toISOString(), title: title, content: content }
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: "images/Freelancer.png",
+    creator: {
+      name: "Arthur"
+    }
   });
+  post
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: "Post created",
+        post: post
+      });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error("Could not find post");
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({ message: "Post fetched", post: post });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
