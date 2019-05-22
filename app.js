1858c83f52a24b23e7c6331884053a672c5cd4cf
ArturoTorresMartinez <efrainarturotorres@gmail.com>
2019-05-22 15:39:52 -0500

    CRUD REST API

--- app.js
+++ app.js
@@ -2,17 +2,45 @@ const express = require("express");
 const feedRoutes = require("./routes/feed");
 const bodyParser = require("body-parser");
 const path = require("path");
+const multer = require("multer");
 const mongoose = require("mongoose");
 const app = express();
+const uuidv4 = require("uuid/v4");
+const cors = require("cors");
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, uuidv4() + file.originalname);
+  }
+});
 
-app.use(bodyParser.json());
-app.use("/images", express.static(path.join(__dirname, "images")));
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+app.options("*", cors());
+app.use(cors());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Contro-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
+app.use(bodyParser.json());
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
+app.use("/images", express.static(path.join(__dirname, "images")));
 app.use("/feed", feedRoutes);
 app.use((error, req, res, next) => {
   console.log(error);
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const path = require("path");
 const { validationResult } = require("express-validator/check");
 const Post = require("../models/post");
 
@@ -21,12 +23,18 @@ exports.createPost = (req, res, next) => {
     error.statusCode = 422;
     throw error;
   }
+  if (!req.file) {
+    const error = new Error("No image provided");
+    error.statusCode = 422;
+    throw error;
+  }
+  const imageUrl = req.file.path.replace("\\", "/");
   const title = req.body.title;
   const content = req.body.content;
   const post = new Post({
     title: title,
     content: content,
-    imageUrl: "images/Freelancer.png",
+    imageUrl: imageUrl,
     creator: {
       name: "Arthur"
     }
@@ -65,4 +73,78 @@ exports.getPost = (req, res, next) => {
       }
       next(err);
     });
+};
+
+exports.updatePost = (req, res, next) => {
+  const postId = req.params.postId;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation Failed, entered data incorrect");
+    error.statusCode = 422;
+    throw error;
+  }
+  const title = req.body.title;
+  const content = req.body.content;
+  let imageUrl = req.body.image;
+  if (req.file) {
+    imageUrl = req.file.path.replace("\\", "/");
+  }
+  if (!imageUrl) {
+    const error = new Error("No file picked");
+    error.statusCode = 422;
+    throw error;
+  }
+  Post.findById(postId)
+    .then(p => {
+      if (!p) {
+        const error = new Error("Could not find post");
+        error.statusCode = 404;
+        throw error;
+      }
+      if (imageUrl !== p.imageUrl) {
+        clearImage(p.imageUrl);
+      }
+      p.title = title;
+      p.imageUrl = imageUrl;
+      p.content = content;
+      return p.save();
+    })
+    .then(r => {
+      res.status(200).json({ message: "Post updated", post: r });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.deletePost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then(p => {
+      if (!p) {
+        const error = new Error("Could not find post");
+        error.statusCode = 404;
+        throw error;
+      }
+      clearImage(p.imageUrl);
+      return Post.findByIdAndRemove(postId);
+    })
+    .then(r => {
+      console.log(r);
+      res.status(200).json({ message: "Deleted post" });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+const clearImage = filePath => {
+  filePath = path.join(__dirname, "..", filePath);
+  fs.unlink(filePath, err => console.log(err));
 };
