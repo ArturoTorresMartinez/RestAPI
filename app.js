a542dd659c47face278fab520fdbdabc6304dba3
ArturoTorresMartinez <efrainarturotorres@gmail.com>
2019-05-24 16:29:00 -0500

    GraphQL User insertion

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const multer = require("multer");
-const graphqlHttp = require('express-graphql');
-const graphqlSchema = require('./graphql/schema');
-const graphqlResolver = require('./graphql/resolver');
+const graphqlHttp = require("express-graphql");
+const graphqlSchema = require("./graphql/schema");
+const graphqlResolver = require("./graphql/resolver");
 const mongoose = require("mongoose");
 const app = express();
 const uuidv4 = require("uuid/v4");
@@ -43,11 +43,23 @@ app.use(
   multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
 );
 app.use("/images", express.static(path.join(__dirname, "images")));
-app.use('/graphql', graphqlHttp({
-  schema: graphqlSchema,
-  rootValue: graphqlResolver,
-  graphiql: true
-}));
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+    graphiql: true,
+    formatError(err) {
+      if (!err.originalError) {
+        return err;
+      }
+      const data = err.originalError.data;
+      const message = err.message || "An error occured";
+      const code = err.originalError.code || 500;
+      return { message: message, statis: code, data: data };
+    }
+  })
+);
 app.use((error, req, res, next) => {
   console.log(error);
   const status = error.statusCode || 500;
